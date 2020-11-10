d43e890427a7342f6dfb036bb181a9f6019742d9
Kris Renaldi <krenaldi@comcast.net>
2020-11-10 17:43:53 -0500

    Refactored CategoryMenu, ProductList, Detail & Home components to use Global State

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions'
+import { useStoreContext } from '../../utils/GlobalState';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_CATEGORIES } from "../../utils/queries";
 
-function CategoryMenu({ setCategory }) {
+function CategoryMenu() {
+  const [ state, dispatch ] = useStoreContext();
+  const { categories } = state;
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+  // const categories = categoryData?.categories || [];
+
+  useEffect(() => {
+    // if categoryData exists or has changed from response of useQuery then tun dispatch()
+    if (categoryData) {
+      // execute dispatch func w/ action obj indicating type of action & data to set state for categories
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]);
+
+  const handleClick = id => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    })
+  }
 
   return (
     <div>
@@ -13,7 +35,7 @@ function CategoryMenu({ setCategory }) {
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            handleClick(item._id);
           }}
         >
           {item.name}
