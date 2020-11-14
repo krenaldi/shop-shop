613d2a2a795e11bd67073c8414ee129184f6e2fb
Kris Renaldi <krenaldi@comcast.net>
2020-11-14 18:39:00 -0500

    Added IndexedDB to allow for cart to work offline

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -4,6 +4,8 @@ import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
+import { idbPromise } from '../../utils/helpers';
+
 function ProductItem(item) {
   const {
     image,
@@ -26,12 +28,17 @@ function ProductItem(item) {
         type: UPDATE_CART_QUANTITY,
         _id: _id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      })
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...item, purchaseQuantity: 1 }
-      })
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
   }
 
